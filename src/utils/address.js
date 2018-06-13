82c2b2a4342795414ce3d08efb4db14ba02dae7f
ming lee <hi@waphe.com>
2018-06-13 17:31:14 +0800

    adddress 前缀更新

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,43 +1,39 @@
-var crypto = require('crypto')
-var base58check = require('./base58check')
-
-const NORMAL_PREFIX = 'A' // A
+var crypto = require('crypto');
+var base58check = require('./base58check');
+var SercJS = require('Serc-js');
+// const NORMAL_PREFIX = 'A' // A
 
 module.exports = {
   isAddress: function (address) {
     if (typeof address !== 'string') {
       return false
     }
-    // if (!/^[0-9]{1,20}$/g.test(address)) {
-      if (!base58check.decodeUnsafe(address.slice(1))) {
-        return false
-      }
-      if (['A'].indexOf(address[0]) == -1) {
-        return false
-      }
-    // }
-    return true
+
+    if (address.indexOf(SercJS.constants.address.prefix) != 0) { // ['A'].indexOf(address[0])
+      return false
+    }
+
+    return base58check.decodeUnsafe(address.slice(SercJS.constants.address.prefix.length));
   },
 
   isBase58CheckAddress: function (address) {
     if (typeof address !== 'string') {
       return false
     }
-    if (!base58check.decodeUnsafe(address.slice(1))) {
+
+    if (address.indexOf(SercJS.constants.address.prefix)) { // ['A'].indexOf(address[0]) == -1
       return false
     }
-    if (['A'].indexOf(address[0]) == -1) {
-      return false
-    }
-    return true
+
+    return base58check.decodeUnsafe(address.slice(SercJS.constants.address.prefix.length));
   },
 
   generateBase58CheckAddress: function (publicKey) {
     if (typeof publicKey === 'string') {
       publicKey = Buffer.from(publicKey, 'hex')
     }
-    var h1 = crypto.createHash('sha256').update(publicKey).digest()
-    var h2 = crypto.createHash('ripemd160').update(h1).digest()
-    return NORMAL_PREFIX + base58check.encode(h2)
+    var h1 = crypto.createHash('sha256').update(publicKey).digest();
+    var h2 = crypto.createHash('ripemd160').update(h1).digest();
+    return SercJS.constants.address.prefix + base58check.encode(h2);
   },
 }
